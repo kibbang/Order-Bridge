3e130ee4bccd60498c3983072d48c5c8f007588a
kibbang <007kibbang@gmail.com>
2026-05-22 21:42:13 +0900

    sql 파일 간격조정

--- src/main/resources/db/migration/V7__insert_channels.sql
+++ src/main/resources/db/migration/V7__insert_channels.sql
@@ -1,5 +1,5 @@
 INSERT INTO channels (name, type, api_key, is_active, created_at, updated_at)
 VALUES
-    ('쿠팡',        'COUPANG',     'mock-coupang-api-key',     TRUE, NOW(), NOW()),
-    ('지마켓',      'GMARKET',      'mock-cafe24-api-key',      TRUE, NOW(), NOW());
-    ('옥션',      'AUCTION',      'mock-auction-api-key',      TRUE, NOW(), NOW());
+    ('쿠팡','COUPANG', 'mock-coupang-api-key', TRUE, NOW(), NOW()),
+    ('지마켓','GMARKET', 'mock-cafe24-api-key', TRUE, NOW(), NOW());
+    ('옥션', 'AUCTION', 'mock-auction-api-key', TRUE, NOW(), NOW());
